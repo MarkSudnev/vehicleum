dd3c8a7a4ce001ad0de303840ed2c4f55a9b2b05
MarkSudnev <m.sudnev@gmail.com>
2022-11-14 01:11:33 +0100

    Add implicit flow controller

---
+++ src/main/java/pl/flathumor/vehicleum/authentication/AuthenticationController.java
@@ -0,0 +1,22 @@
+package pl.flathumor.vehicleum.authentication;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@Slf4j
+@RestController
+@RequestMapping("/authentication")
+public class AuthenticationController {
+
+  @GetMapping("/implicit/callback")
+  @ResponseStatus(OK)
+  public void implicit(@RequestParam("access_token") final String token) {
+    log.info("Got token: " + token);
+  }
+}
